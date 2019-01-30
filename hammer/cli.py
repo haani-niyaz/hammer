d25fb5a360c69f97ed642c6490f9dffbcea6b1d3
Haani Niyaz <haani.niyaz@team.telstra.com>
2019-01-30 19:34:37 +1100

    Repo setup and initialization

---
+++ hammer/cli.py
@@ -0,0 +1,8 @@
+import os
+import sys
+import urllib3
+from kubernetes import client, config, watch
+
+
+def main():
+  print('works')
---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from hammer import __version__
+from setuptools import setup, find_packages
+
+setup(
+    name='hammer',
+    version=__version__,
+    author='Haani Niyaz',
+    author_email='haani.niyaz@gmail.com',
+    url='https://github.com/haani-niyaz/hammer',
+    license='MIT',
+    description='Utility tool for kubernetes',
+    keywords='automation helper',
+    packages=find_packages(exclude=['docs', 'tests']),
+    entry_points={
+        'console_scripts': [
+            'hammer=hammer:main',
+        ],
+    },
+
+)
