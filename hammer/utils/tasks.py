83ad06459a44983249ba9eebff35afcf20627c1e
Haani Niyaz <haani.niyaz@team.telstra.com>
2019-02-03 17:15:44 +1100

    JSON to python dictionary function

--- hammer/utils/tasks.py
+++ hammer/utils/tasks.py
@@ -11,3 +11,23 @@ import json
 class TasksError(Exception):
   """An exception that occurs when performing administrative operations"""
   pass
+
+
+def json_to_dict(data):
+  """Covert json data to python dict
+
+  Args:
+    data (json): json paylod
+
+  Returns:
+    dict: json payload as a python dictionary
+
+  Raises:
+    TaskError: for any errors when attempting to convert json data to python dictionary
+
+  """
+  try:
+    return json.loads(data)
+  # Malformed json
+  except TypeError as e:
+    raise TasksError("Failed to load json data: {}".format(e))
