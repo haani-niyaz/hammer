f32d5db395273b74af92638e93aa774db2853a79
Haani Niyaz <haani.niyaz@team.telstra.com>
2019-02-05 19:36:00 +1100

    Get flex volume 'block storage volume' id

--- hammer/__main__.py
+++ hammer/__main__.py
@@ -1,5 +1,6 @@
 import argparse
 import sys
+import textwrap
 from hammer import __version__ as VERSION
 from .actions import kube
 
@@ -17,11 +18,21 @@ subparsers = parser.add_subparsers(
 parser_bsv = subparsers.add_parser(
     'get-bsv',
     formatter_class=argparse.RawDescriptionHelpFormatter,
-    help='find block storage volume id for a given pod')
+    help='find block storage volume id for a given pod',
+    description=textwrap.dedent('''Description:
+  Retrieve the PersistentVolume BSV id from a Pod name 
+  in a given namespace with a target PersistentVolumeClaim 
+  name.
+  
+  By default the Kubernetes 'default' namespace is used 
+  with PersistentVolumeClaim name 'home'.
+
+      ''')
+)
 parser_bsv.add_argument('pod', help='name of pod')
 parser_bsv.add_argument('-n', '--namespace', default='default',
                         help='Pod namespace')
-parser_bsv.add_argument('-a', '--pvc', default='home',
+parser_bsv.add_argument('-c', '--pvc', default='home',
                         help='persistent volume claim name')
 
 
@@ -35,17 +46,19 @@ if cli.sub_cmd == 'get-bsv':
     print(e.message)
     sys.exit(1)
 
-  # if cli.verbose:
-  #   print(pod.pvc_names)
-
-  # Get PV for PVC home
+  # Get PV name for PVC name
   pvc_name = pod.pvc_names.get(cli.pvc)
+
+  # Get block storage volume ID
   if pvc_name:
     pvc = kube.PersistentVolumeClaim(pvc_name, cli.namespace)
     pv = kube.PersistentVolume(pvc.pv_name)
     flex_volume_id = pv.get_flex_volume_id('BsvId')
     if flex_volume_id:
       print(flex_volume_id)
+    else:
+      print('No bsv found')
+      sys.exit(1)
   else:
-    print("No 'home' persistent volume claim")
+    print("No {} persistent volume claim".format(cli.pvc))
     sys.exit(1)
